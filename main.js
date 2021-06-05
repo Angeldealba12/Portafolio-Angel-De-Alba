9074b326b44bd3429ddb85e82e8e94a83ed937aa
Angel <angel.dealba.garcia@gmail.com>
2021-06-05 18:40:35 -0500

    Actualizando menu responsive

---
+++ main.js
@@ -0,0 +1,9 @@
+// MENU HAM ACTIVATION IN RESPONIVE
+
+ const ham = document.querySelector('.ham');
+ const enlaces = document.querySelector('.menu');
+
+ ham.addEventListener('click', () => {
+     enlaces.classList.toggle('activado');
+ });
+
